ab39e88401348e072f4a39f2437f13233c9a9fec
satvik <satvikshukla955923@gmail.com>
2026-01-04 14:34:27 +0530

    chore: register http tool in builtin tools

--- peargent/tools/__init__.py
+++ peargent/tools/__init__.py
@@ -2,28 +2,18 @@
 
 from .math_tool import MathTool
 from .text_extraction_tool import TextExtractionTool
-<<<<<<< HEAD
 from .wikipedia_tool import WikipediaKnowledgeTool
-
+from .http_tools import HttpTool
 calculator = MathTool()
 text_extractor = TextExtractionTool()
 wikipedia_tool = WikipediaKnowledgeTool()
-=======
-from .http_tools import HttpTool
-
-calculator = MathTool()
-text_extractor = TextExtractionTool()
 http_client = HttpTool()
->>>>>>> f1eb8ac (Add HTTP client tool  with retries,timeout,header support)
 
 BUILTIN_TOOLS = {
     "calculator": calculator,
     "extract_text": text_extractor,
-<<<<<<< HEAD
     "search_wikipedia": wikipedia_tool,
-=======
     "http_client": http_client,
->>>>>>> f1eb8ac (Add HTTP client tool  with retries,timeout,header support)
 }
 
 def get_tool_by_name(name: str):
